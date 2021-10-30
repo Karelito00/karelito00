42143d49b094cc3d56c2cd3e9093c8ec90f3d9bd
Karelito00 <karelkdv2000@gmail.com>
2021-10-30 15:56:28 -0400

    Change homepage

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:karelito00/cv-karel.git master:gh-pages
+git push -f git@github.com:karelito00/karelito00.git master:gh-pages
 cd -
